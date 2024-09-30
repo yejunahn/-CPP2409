b4872bdd5ed74671acc683295d7d298ba5b58a98
yejunahn <dksdulwns1@naver.com>
2024-09-30 23:02:57 +0900

    Array Exercise

--- practice/week5/pyramid.cpp
+++ practice/week5/pyramid.cpp
@@ -6,7 +6,7 @@ int main() {
     cout <<  "몇 층을 쌓겠습니까? (5~100):";
     cin >> floor;
     for(int i=0 ; i< floor; i++) {
-        for (int j =0; j<floor-1-i; j++){
+        for (int j =floor-1-i; j>0; j--){
             cout << "S";
         }
         for (int k =0 ; k < i*2-1; k++){
